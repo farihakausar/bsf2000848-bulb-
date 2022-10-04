fbfd58ce7f49cecffd6d81026524261d4861c39c
farihakausar <88570666+farihakausar@users.noreply.github.com>
2022-10-04 10:27:31 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,26 @@
+var it = document.getElementById("hi")
+var its = document.getElementById("hey")
+changeStateOn=()=>{
+  
+    its.src="bulbOn.jpg";
+    console.log("change");
+
+}
+changeStateOff=()=>{
+  
+
+it.src="bulbOff.jpg";
+console.log("change");
+
+
+}
+
+
+// function changeImage() {
+//     if (document.getElementById("imgClickAndChange").src == "http://www.userinterfaceicons.com/80x80/minimize.png"){
+//         document.getElementById("imgClickAndChange").src = "http://www.userinterfaceicons.com/80x80/maximize.png";
+//     } else {
+//         document.getElementById("imgClickAndChange").src = "http://www.userinterfaceicons.com/80x80/minimize.png";
+//     }
+// }
+
